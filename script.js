17653a2fd0ae48e6d99059f8f7acd95fd24fee3b
nehalskdev <nikesk1999@gmail.com>
2024-10-05 15:39:44 +0530

    removed white space in file

--- script.js
+++ script.js
@@ -7,18 +7,18 @@ const input = document.querySelector("input");
 addBtn.addEventListener("click", (e) => {
   const markup = `
         <div class="new-element">
-            <h3 class="dlt-el">☑️${(input.value)}</h3>
+            <h3 class="dlt-el">☑️${input.value}</h3>
             
         </div>`;
-    if (!input.value == '') {addBtn.insertAdjacentHTML("afterend", markup)}; 
-
-    
+  if (!input.value == "") {
+    addBtn.insertAdjacentHTML("afterend", markup);
+  }
 });
 
 // deleting element
 
 document.body.addEventListener("click", (e) => {
   if (e.target.classList.contains("dlt-el")) {
-      e.target.remove();
-    };
+    e.target.remove();
+  }
 });
