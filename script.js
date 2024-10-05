8ea42704a6677c4e9fd9575cbc75f49764b34293
nehalskdev <nikesk1999@gmail.com>
2024-10-05 15:18:15 +0530

    added files

---
+++ script.js
@@ -0,0 +1,22 @@
+"strict mode";
+
+const addBtn = document.querySelector("button");
+const input = document.querySelector("input");
+
+// adding element
+addBtn.addEventListener("click", (e) => {
+  const markup = `
+        <div class="new-element">
+            <h3 class="dlt-el">☑️${input.value}</h3>
+            
+        </div>`;
+  addBtn.insertAdjacentHTML("afterend", markup);
+});
+
+// deleting element
+
+document.body.addEventListener("click", (e) => {
+  if (e.target.classList.contains("dlt-el")) {
+      e.target.remove();
+    };
+});
